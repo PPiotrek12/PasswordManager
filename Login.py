a44445d245b53cf868749cba24d0997a567eff65
PPiotrek12 <PPiotrek12@github.com>
2022-09-22 19:07:00 +0200

    .

--- Login.py
+++ Login.py
@@ -2,11 +2,13 @@ import hashlib
 import values
 
 def check():
-    password = input("Insert your password: ")
+    password = input("Insert your password.\n>>> ")
     hash = hashlib.new('sha256')
     hash.update(password.encode())
     return values.login_password_hash == hash.digest()
 def login():
+    return True # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
     passed = False
     for i in range(3):
         correct = check()
@@ -14,9 +16,9 @@ def login():
             passed = True
             break
         else:
-            print("Invalid password, try again")
+            print("Invalid password, try again.\n")
     if not passed:
-        print("You inserted invalid password three times.")
+        print("You inserted invalid password three times.\n")
         return False
-    print("You are logged in successfully.")
+    print("You are logged in successfully.\n")
     return True
--- main.py
+++ main.py
@@ -5,4 +5,15 @@ def start():
 	if not login():
 		return
 
+	key1 = input("Insert first encription key.\n>>> ")
+	print("\n")
+
+	choice = input("""What do you want to do?
+[1] Add new account.
+[2] See login and password to the existing account.
+[3] Change an existing account
+[4] Remove an existing account.
+>>> """)
+
+	
 start()
